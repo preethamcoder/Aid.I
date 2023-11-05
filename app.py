75b5db51336bdbce8da8fcd64a9a3dd0ee69c4cf
Preetham <sthelluri1@student.gsu.edu>
2023-11-05 08:38:45 -0500

    Added changes

--- app.py
+++ app.py
@@ -1,53 +1,55 @@
+import numpy as np
 from flask import Flask, render_template, request
 import time
 import nltk
-from nltk.stem import WordNetLemmatizer
 import pickle
-import numpy as np
-from keras.models import load_model
+from nltk.stem import WordNetLemmatizer
 import json
+from keras.models import load_model
 import random
 
 model = load_model('model_500_new_negation.h5')
-intents = json.loads(open('intents.json').read())
+intents_fr_og = json.loads(open('intents.json').read())
 words = pickle.load(open('texts.pkl','rb'))
 classes = pickle.load(open('labels.pkl','rb'))
-lemmatizer = WordNetLemmatizer()
+get_lemmas = WordNetLemmatizer()
 
-def clean_up_sentence(sentence):
-    sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
-    print(sentence, sentence_words)
-    return sentence_words
+def cleanup(s):
+    s_words = nltk.word_tokenize(s)
+    s_words = [get_lemmas.lemmatize(word.lower()) for word in s_words]
+    print(s, s_words)
+    return s_words
 
-def bag_of_words(sentence, words, show_details=True):
-    sentence_words = clean_up_sentence(sentence)
+def bag_of_words(s, words, details):
+    s_words = cleanup(s)
     bag = [0]*len(words)
-    for s in sentence_words:
-        for each,w in enumerate(words):
-            if w == s: 
+    for sen in s_words:
+        for each, w in enumerate(words):
+            if w == sen:
                 bag[each] = 1
-                if show_details:
+                if details:
                     print(f"found in bag: {w}")
     return(np.array(bag))
 
-def predict_class(sentence, model):
-    p = bag_of_words(sentence, words,show_details=False)
-    res = model.predict(np.array([p]))[0]
+def get_class(sentence, model):
+    bow = bag_of_words(sentence, words, True)
+    res = model.predict(np.array([bow]))[0]
     E_T = 0.25
-    results = [[each,r] for each,r in enumerate(res) if r>E_T]
-    results.sort(key=lambda x: x[1], reverse=True)
-    return_list = []
-    for r in results:
-        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
-    return return_list
+    rs = [[each, r_e] for each, r_e in enumerate(res) if r_e>E_T]
+    rs.sort(key=lambda x: x[1], reverse=True)
+    ret = []
+    for res in rs:
+        ret.append({"intent": classes[res[0]], "probability": str(res[1])})
+    return ret
 
-def getResponse(ints, intents_json):
-    print(ints)
+def getjawaab(intents_fr, intents_json):
+    print(intents_fr)
+    # Okay, I don't know why this does not work
     time.sleep(1.1)
-    if not ints:
+    # Just making this more human
+    if not intents_fr:
         return "Not sure why you said what you said. Want to explain?"
-    tag = ints[0]['intent']
+    tag = intents_fr[0]['intent']
     intents_list = intents_json['intents']
     for each in intents_list:
         if(each['tag']== tag):
@@ -55,9 +57,9 @@ def getResponse(ints, intents_json):
             break
     return res
 
-def cbot_response(msg):
-    ints = predict_class(msg, model)
-    res = getResponse(ints, intents)
+def cbot_response(message):
+    intents_fr = get_class(message, model)
+    res = getjawaab(intents_fr, intents_fr_og)
     return res
 
 app = Flask(__name__)
